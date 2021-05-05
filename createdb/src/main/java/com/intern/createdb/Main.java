8ded6414861a7245ac468ef0a1c73933912b48fc
Laziz-R <rohataliyevlaziz@gmail.com>
2021-05-05 22:32:46 +0500

    duvay

--- createdb/src/main/java/com/intern/createdb/Main.java
+++ createdb/src/main/java/com/intern/createdb/Main.java
@@ -1,13 +1,27 @@
 package com.intern.createdb;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+public class Main 
 {
-    public static void main( String[] args )
+    final static String URL = "jdbc:postgresql://localhost:5432/";
+    final static String USER = "postgres";
+    final static String PASS = "6688";
+    static String path = System.getProperty("user.dir")+"/src/main/java/recources/";
+
+    public static void main( String[] args ) throws SQLException, FileNotFoundException
     {
-        System.out.println( "Hello World!" );
+        Connection con = DriverManager.getConnection(URL, USER, PASS);
+        ScriptRunner sr = new ScriptRunner(con);
+        Reader reader = new BufferedReader(new FileReader(path+"createDB.sql"));
+        sr.runScript(reader);
     }
 }
