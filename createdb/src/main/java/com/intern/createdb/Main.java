d4f1042cd31b5fa79dde879bea17b2f309bf6154
R-Laziz <rohataliyevlaziz@gmail.com>
2021-05-06 16:14:15 +0500

    duvay

--- createdb/src/main/java/com/intern/createdb/Main.java
+++ createdb/src/main/java/com/intern/createdb/Main.java
@@ -2,23 +2,30 @@ package com.intern.createdb;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.Scanner;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class Main {
-    final static String USER = "postgres";
-    final static String PASS = "6688";
-    static String URL = "jdbc:postgresql://localhost:5432/";
     static String path = System.getProperty("user.dir") + "/createdb/src/main/java/resources/";
 
-    public static void main(String[] args) throws SQLException, FileNotFoundException, InterruptedException {
+    public static void main(String[] args) throws SQLException, InterruptedException, IOException {
+        Properties props = new Properties();
+        props.load(new FileInputStream(path+"config.properties"));
+
+        final String URL = props.getProperty("URL");
+        final String USER = props.getProperty("USER");
+        final String PASS = props.getProperty("PASS");
+
         Scanner sc = new Scanner(System.in);
         Connection con = DriverManager.getConnection(URL, USER, PASS);
 
